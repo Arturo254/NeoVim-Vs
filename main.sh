f5338cc2b0df0869244d7104bdbabf4e08ef45d1
Arturo254 <87346871+Arturo254@users.noreply.github.com>
2023-08-04 18:45:28 -0600

    Add files via upload

---
+++ main.sh
@@ -0,0 +1,23 @@
+#!bin/bash
+
+#!/bin/bash
+
+# Detectar si estamos en Termux o Linux
+if [ -d "/data/data/com.termux" ]; then
+    # Estamos en Termux
+    echo "Detectado Termux"
+    pkg install neovim git zsh -y 
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc
+
+else
+    # Asumimos que estamos en Linux
+    echo "Detectado Linux"
+    if [ "$EUID" -ne 0 ]; then
+        echo "Por favor, ejecuta este script como root"
+        exit
+    fi
+    apt-get install neovim git zsh -y 
+    git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ~/powerlevel10k
+echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc 
+fi
